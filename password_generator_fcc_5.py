4a62d0867f1904b5a2bb841c39b0f67d46492e74
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-03-16 23:22:56 +0300

    learning regular expressions

---
+++ password_generator_fcc_5.py
@@ -0,0 +1,39 @@
+import re
+import secrets
+import string
+
+# for constraint, pattern in constraints:
+        #     if constraint <= len(re.findall(pattern, password)):
+        #         count += 1
+            
+        # if count == 4:
+
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+       
+        constraints = [
+            (nums, r'\d'),
+            (lowercase, r'[a-z]'),
+            (uppercase, r'[A-Z]'),            
+            (special_chars, fr'[{symbols}]')            
+        ]
+
+        # Check constraints
+        count = 0
+
+    return password
+
+# new_password = generate_password(8)
+# print(new_password)
