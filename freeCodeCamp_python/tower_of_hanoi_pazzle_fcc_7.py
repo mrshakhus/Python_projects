526ca2bef989b605708f26cc0e52e509d245abe1
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-03-19 23:14:31 +0300

    started making tower of Hanoi pazzle

---
+++ freeCodeCamp_python/tower_of_hanoi_pazzle_fcc_7.py
@@ -0,0 +1,13 @@
+NUMBER_OF_DISKS = 3
+number_of_moves = 2**NUMBER_OF_DISKS - 1
+rods = {
+    'A': list(range(NUMBER_OF_DISKS, 0, -1)),
+    'B': [],
+    'C': []
+}
+
+def move(n, source, auxiliary, target):
+    print(rods)
+
+move()
+#git
