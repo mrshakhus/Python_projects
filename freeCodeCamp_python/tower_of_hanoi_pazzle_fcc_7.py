c149070583ccc0d3b83b8131e989c505de3d7db7
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-03-20 00:32:57 +0300

    pazzle in progress

--- freeCodeCamp_python/tower_of_hanoi_pazzle_fcc_7.py
+++ freeCodeCamp_python/tower_of_hanoi_pazzle_fcc_7.py
@@ -5,9 +5,15 @@ rods = {
     'B': [],
     'C': []
 }
-
 def move(n, source, auxiliary, target):
+    # display starting configuration
     print(rods)
+    for i in range(number_of_moves):
+        remainder = (i + 1) % 3
+        if remainder == 1:
+            print(f'Move {i + 1} allowed between {source} and {target}')
+        elif remainder == 2:
+            print(f'Move {i + 1} allowed between {source} and {auxiliary}')
 
-move()
-#git
+# initiate call from source A to target C with auxiliary B
+move(NUMBER_OF_DISKS, 'A', 'B', 'C')
