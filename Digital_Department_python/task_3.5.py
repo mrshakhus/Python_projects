99935c72844f0638924aa98a2ca4c59b8d13f8c2
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-03-27 22:39:22 +0300

    task with randint complited

--- Digital_Department_python/task_3.5.py
+++ Digital_Department_python/task_3.5.py
@@ -2,18 +2,34 @@
 
 from random import randint
 
-random_numbers = []
+def duplicate_iteration():
+    random_numbers = []
+
+    for _ in range(10):
+        random_numbers.append(randint(1,10))
+
+    iteration_number = 0
+    index = 0
+    counter = 0
+    is_break = False
+    while index != 10:
+        number_from_list = random_numbers[index]
+        for index_to_check in range(10):
+            if index == index_to_check:
+                continue
+            elif number_from_list == random_numbers[index_to_check]:
+                iteration_number = index_to_check - index 
+                is_break = True
+                break
+        if is_break:
+            break
+        if counter == 100:
+            break
+        counter += 1
+
+    return iteration_number
 
 for _ in range(10):
-    random_numbers.append(randint(1,10))
+    print(duplicate_iteration())
 
-iteration_number = 0
-index = 0
-while index != 10:
-    number_from_list = random_numbers[index]
-    for index_to_check in range(10):
-        if index == index_to_check:
-            continue
-        elif number_from_list == random_numbers[index_to_check]:
-            iteration_number = index_to_check - index 
-
+#not an elegant solution, but nonetheless
