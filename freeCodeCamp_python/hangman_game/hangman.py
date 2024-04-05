b3bb295a54add29746a3450eaa1c7f13cb6755dd
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-04-05 22:48:28 +0300

    hangman completed

--- freeCodeCamp_python/hangman_game/hangman.py
+++ freeCodeCamp_python/hangman_game/hangman.py
@@ -1,5 +1,32 @@
 from random_words_for_hangman import random_words
 import random
 
-some_word = random_words[random.randrange(len(random_words))]
-print(some_word)
+guessed_word = random_words[random.randrange(len(random_words))].upper()
+length_of_guessed = len(guessed_word)
+lives = length_of_guessed
+used_letters = ''
+current_word = ['-']*length_of_guessed
+
+
+while True:
+    print(f'You have {lives} lives left and you used these letters: {used_letters}')
+    print(f'Current word: ', end='')
+    for letter in current_word:
+        print(f'{letter} ', end='')
+    
+    user_letter = input('\n\nGuess a letter: ').upper()
+
+    if user_letter in guessed_word:
+        print('\n')
+        for index in range(length_of_guessed):
+            if user_letter == guessed_word[index]:
+                current_word[index] = user_letter
+    else:
+        lives -= 1
+        print(f'\nYour letter, {user_letter} is not in the word')
+
+    if lives == 0:
+        print(f'Sorry, you died. The word was {guessed_word}')
+        break
+
+    used_letters += f' {user_letter}'    
