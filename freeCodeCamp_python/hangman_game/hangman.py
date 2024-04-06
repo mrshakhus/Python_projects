80d7671db9682987b998cb93461c4d33d9274ad3
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-04-06 21:05:45 +0300

    changed a code

--- freeCodeCamp_python/hangman_game/hangman.py
+++ freeCodeCamp_python/hangman_game/hangman.py
@@ -1,32 +1,50 @@
 from random_words_for_hangman import random_words
 import random
+import string
 
-guessed_word = random_words[random.randrange(len(random_words))].upper()
-length_of_guessed = len(guessed_word)
-lives = length_of_guessed
-used_letters = ''
-current_word = ['-']*length_of_guessed
-
-
-while True:
-    print(f'You have {lives} lives left and you used these letters: {used_letters}')
-    print(f'Current word: ', end='')
-    for letter in current_word:
-        print(f'{letter} ', end='')
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
     
-    user_letter = input('\n\nGuess a letter: ').upper()
+    return word.upper()
 
-    if user_letter in guessed_word:
-        print('\n')
-        for index in range(length_of_guessed):
-            if user_letter == guessed_word[index]:
-                current_word[index] = user_letter
-    else:
-        lives -= 1
-        print(f'\nYour letter, {user_letter} is not in the word')
+
+def hangman():
+    guessed_word = get_valid_word(random_words)
+    lives = 7
+
+    alphabet = set(string.ascii_uppercase)
+    letters_from_guessed = set(guessed_word)
+    used_letters = set()
+
+    while letters_from_guessed != None or lives != 0:
+        print(f'You have {lives} lives left and you used these letters:', ' '.join(used_letters))
+
+        current_word_list = [letter if letter in used_letters else '-' for letter in guessed_word]
+        print(f'Current word: ', ''.join(current_word_list))
+        
+        user_letter = input('\nGuess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+
+            if user_letter in guessed_word:
+                letters_from_guessed.remove(user_letter)
+            
+            else:
+                lives -= 1
+                print(f'Your letter, {user_letter} is not in the word')
+
+        elif user_letter in used_letters:
+            print('You already used that letter. Try another.')
+
+        else:
+            print("It's not a valid letter")
 
     if lives == 0:
         print(f'Sorry, you died. The word was {guessed_word}')
-        break
-
-    used_letters += f' {user_letter}'    
+    else:
+        print(f"Excelsior! You guessed it! It's {guessed_word}")
+      
+if __name__ == '__main__':
+    hangman()
