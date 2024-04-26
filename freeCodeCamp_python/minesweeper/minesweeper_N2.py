8aa25af7a1c4689b069e433182a46eaa13cc9605
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-04-27 01:47:57 +0300

    working minesweeper

--- freeCodeCamp_python/minesweeper/minesweeper_N2.py
+++ freeCodeCamp_python/minesweeper/minesweeper_N2.py
@@ -10,6 +10,25 @@ class Minesweeper:
         self.dug = set()
 
         self.board = self.make_new_board()
+        self.assign_values_to_board()
+
+    def __str__(self):
+        visible_board = [[' ' for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+
+        for row in range(self.dim_size):
+            for col in range(self.dim_size):
+                if (row, col) in self.dug:
+                    visible_board[row][col] = str(self.board[row][col])
+
+        col_numbers = [str(i) for i in range(self.dim_size)]
+        board_for_print = '   '+'  '.join(col_numbers)+' \n'
+        board_for_print += f'{'-'*(3*self.dim_size + 5)}\n'
+        for row_number, row_print in enumerate(visible_board):
+            board_for_print += str(row_number)+ ' |' + ' |'.join(row_print) + ' |\n'
+
+        board_for_print += f'{'-'*(3*self.dim_size + 5)}\n'
+
+        return board_for_print
         
 
     def make_new_board(self):
@@ -21,14 +40,12 @@ class Minesweeper:
             row = coord // 10
             col = coord % 10
 
-            if self.board[row][col] == '*':
+            if board[row][col] == '*':
                 continue
 
-            self.board[row][col] = '*'
+            board[row][col] = '*'
 
             count += 1
-
-        self.assign_values_to_board
 
         return board
     
@@ -48,7 +65,8 @@ class Minesweeper:
             for col in range(max(0, c-1), min((c+1)+1, self.dim_size)):
                 if row == r and col == c:
                     continue
-                num_bombs += 1
+                if self.board[row][col] == '*':
+                    num_bombs += 1
 
         return num_bombs
     
@@ -56,6 +74,8 @@ class Minesweeper:
         
         if (r, c) in self.dug:
             return False
+
+        self.dug.add((r,c))
 
         if self.board[r][c] == '*':
             return False
@@ -80,9 +100,39 @@ def play(dim_size=10, bomb_num=10):
     game = Minesweeper(dim_size, bomb_num)
     print(game)
 
-    
+    while len(game.dug) < dim_size ** 2 - bomb_num:
+        coord = re.split(',[\\s]*', input('Where would you like to dig? Input as a row, col: '))
+        row, col = int(coord[0]), int(coord[-1])
 
-    #while len(game.dug) != dim_size - bomb_num:
+        if row < 0 or row >= dim_size or col < 0 or col >= dim_size:
+            print('Invalid numbers. Please, try again')
+            continue
+
+        safe = game.dig(row, col)
+
+        if not safe and game.board[row][col] != '*':
+            print
+
+        if not safe and game.board[row][col] == '*':
+            game.dug = [(r,c) for r in range(dim_size) for c in range(dim_size)]
+            print(game) 
+            break
+        
+        print(game)
+
+    if safe:
+        print('Congratulations! You found all bombs!')
+    else:
+        print('GAME OVER')
+
+if __name__ == '__main__':
+    play()
+
+
+
+
+
+    
 
 
                 
