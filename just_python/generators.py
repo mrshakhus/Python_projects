20e02ebd6fbcf9a16782d3e23475f41967d3b330
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-05-05 23:16:49 +0300

    Fibbonachi numbers with generator

--- just_python/generators.py
+++ just_python/generators.py
@@ -1,13 +1,25 @@
-import random
+# fill in this function
+def fib():
+    a = 1
+    b = 1
+    print(a)
+    print(b)
+    while True:
+        sum = a+b
+        yield sum
+        a = b
+        b = sum
+    
 
-def lottery():
-    # returns 6 numbers between 1 and 40
-    for i in range(6):
-        yield random.randint(1, 40)
-        print('yield:',i)
+# testing code
+import types
+if type(fib()) == types.GeneratorType:
+    print("Good, The fib function is a generator.")
 
-    # returns a 7th number between 1 and 15
-    #yield random.randint(1, 15)
+    counter = 0
+    for n in fib():
+        print(n)
+        counter += 1
+        if counter == 10:
+            break
 
-for random_number in lottery():
-       print("\tAnd the next number is... %d!" %(random_number))
