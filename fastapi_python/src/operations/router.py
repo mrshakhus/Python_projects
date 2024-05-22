d594a7afef3ea8fe19f546599ce994434e20b14c
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-05-22 20:09:50 +0300

    implamenting put and patch methods

--- fastapi_python/src/operations/router.py
+++ fastapi_python/src/operations/router.py
@@ -1,6 +1,6 @@
-
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import insert, select
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy import delete, insert, select, update
 
 from src.database import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -14,25 +14,66 @@ router = APIRouter(
 )
 
 @router.get("/")
-async def get_specific_operation(operation_type: str, session: AsyncSession = Depends(get_async_session)):
-    try:
-        query = select(operation).where(operation.c.type == operation_type)
+async def get_specific_operation(id: int, session: AsyncSession = Depends(get_async_session)):
+    # try:
+        query = select(operation).where(operation.c.id == id)
         result = await session.execute(query)
-        return {
-            "status": "succesful",
-            "data": result.all(),
-            "details": None
-        }
-    except:
-        raise HTTPException(status_code=500, detail={
-            "status": "error",
-            "data": None,
-            "details": None
-        })
+        result = jsonable_encoder(result)
+        print(result)
+        return result
+    #     return {
+    #         "status": "succesful",
+    #         "data": result.all(),
+    #         "details": None
+    #     }
+    # except:
+    #     raise HTTPException(status_code=500, detail={
+    #         "status": "error",
+    #         "data": None,
+    #         "details": None
+    #     })
     
-@router.post("/")
-async def add_specific_operation(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
-    stmt = insert(operation).values(**new_operation.model_dump())
-    await session.execute(stmt)
-    await session.commit()
-    return {"status": "succesfull"}
+# @router.post("/")
+# async def add_specific_operation(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+#     stmt = insert(operation).values(**new_operation.model_dump())
+#     await session.execute(stmt)
+#     await session.commit()
+#     return {"status": "succesfull"}
+
+# @router.put("/{id}")
+# async def replace_spicific_operation(id: int, new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+#     try:
+#         new_operation.id = id
+#         stmt = update(operation).where(operation.c.id == id).values(**new_operation.model_dump())
+#         await session.execute(stmt)
+#         await session.commit()
+#         return {
+#             "status": "succesful",
+#             "data": new_operation,
+#             "details": None
+#         }
+#     except:
+#         raise HTTPException(status_code=500, detail={
+#             "status": "error",
+#             "data": None,
+#             "details": None
+#         })
+
+# @router.delete("/{id}")
+# async def replace_spicific_operation(id: int, session: AsyncSession = Depends(get_async_session)):
+#     try:
+#         query = select(operation).where(operation.c.id == id)
+#         stmt = delete(operation).where(operation.c.id == id)
+#         result = await session.execute(query)
+#         await session.execute(stmt)
+#         return {
+#             "status": "succesful",
+#             "data": result.all(),
+#             "details": None
+#         }
+#     except:
+#         raise HTTPException(status_code=500, detail={
+#             "status": "error",
+#             "data": None,
+#             "details": None
+#         })
