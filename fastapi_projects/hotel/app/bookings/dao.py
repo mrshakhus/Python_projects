41af72a156750d147585ea6b74295036238d1a02
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-06-11 23:51:22 +0300

    making long db queries

--- fastapi_projects/hotel/app/bookings/dao.py
+++ fastapi_projects/hotel/app/bookings/dao.py
@@ -1,6 +1,28 @@
+from datetime import date
 from app.bookings.models import Bookings
 from app.dao.base import BaseDAO
 
 
 class BookingsDAO(BaseDAO):
     model = Bookings
+    
+    @classmethod
+    async def add(
+        cls,
+        room_id: int,
+        date_from: date,
+        date_to: date,
+        ):
+        """
+        WITH booked_rooms AS(
+        SELECT * FROM bookings
+        WHERE room_id = 1 AND
+        (date_from >= '2023-05-15' AND date_from <= '2023-06-20') OR
+        (date_from <= '2023-05-15' AND date_to > '2023-05-15')
+        )
+        SELECT rooms.quantity - COUNT(booked_rooms.room_id) FROM rooms 
+        LEFT JOIN booked_rooms ON booked_rooms.room_id = rooms.id
+        WHERE rooms.id = 1
+        GROUP BY rooms.quantity, booked_rooms.room_id
+        """
+
--- fastapi_projects/hotel/app/bookings/router.py
+++ fastapi_projects/hotel/app/bookings/router.py
@@ -1,3 +1,4 @@
+from datetime import date
 from fastapi import APIRouter, Depends
 
 from app.bookings.dao import BookingsDAO
@@ -13,3 +14,10 @@ router = APIRouter(
 @router.get("", response_model=list[SBooking])
 async def get_bookings(user: Users = Depends(get_current_user)):
     return await BookingsDAO.find_all(id=user.id)
+
+@router.post("")
+async def add_booking(
+    room_id: int, date_from: date, date_to: date,
+    user: Users = Depends(get_current_user),
+):
+    await BookingsDAO.add(user.id, room_id, date_from, date_to)
