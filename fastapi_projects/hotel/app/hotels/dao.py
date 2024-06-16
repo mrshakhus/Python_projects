587c02d065f2054c7501798469e649bee1b5e043
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-06-16 23:52:15 +0300

    learning queries

--- fastapi_projects/hotel/app/hotels/dao.py
+++ fastapi_projects/hotel/app/hotels/dao.py
@@ -8,7 +8,7 @@ from app.database import async_session_maker
 from app.hotels.rooms.models import Rooms
 
 
-class BHotelDAO(BaseDAO):
+class HotelDAO(BaseDAO):
     model = Hotels
 
     @classmethod
@@ -20,38 +20,72 @@ class BHotelDAO(BaseDAO):
     ):
         #first I need to get available rooms in a hotel
         #then check whether there are >= 1 room
-        async with async_session_maker() as session:
-            booked_rooms = select(Bookings).where(
-                and_(
-                    Bookings.room_id == room_id,
-                    or_(
-                        and_(
-                            Bookings.date_from >= date_from,
-                            Bookings.date_from <= date_to
-                        ),
-                        and_(
-                            Bookings.date_from <= date_from,
-                            Bookings.date_to > date_from
-                        )
-                    )
-                )
-            ).cte("booked_rooms")
 
-            get_rooms_left = (
+        #1 Don't know how to do yet
+
+        #2 
+        async with async_session_maker() as session:
+            all_booked_rooms = select(Bookings).where(
+                or_(
+                    and_(
+                        Bookings.date_from >= date_from,
+                        Bookings.date_from <= date_to
+                    ),
+                    and_(
+                        Bookings.date_from <= date_from,
+                        Bookings.date_to > date_from
+                    )                    )
+            ).cte("all_booked_rooms")
+
+            hotel_id_of_booked_rooms = (
                 select(
-                    (Rooms.quantity - func.count(booked_rooms.c.room_id)
-                    .filter(booked_rooms.c.room_id.is_not(None))
+                    (Rooms.quantity - func.count(all_booked_rooms.c.room_id)
+                    .filter(all_booked_rooms.c.room_id.is_not(None))
                     .label("rooms_left"))
                 )
                 .select_from(Rooms)
                 .join(
-                    booked_rooms,
-                    booked_rooms.c.room_id == Rooms.id,
-                    isouter=True
+                    all_booked_rooms,
+                    all_booked_rooms.c.room_id == Rooms.id,
                 )
-                .where(Rooms.id == room_id)
-                .group_by(Rooms.quantity, booked_rooms.c.room_id)
+                .group_by(Rooms.hotel_id)
             )
 
-            rooms_left = await session.execute(get_rooms_left)
+            rooms_left = await session.execute(hotel_id_of_booked_rooms)
             rooms_left: int = rooms_left.scalar()
+
+        # async with async_session_maker() as session:
+        #     booked_rooms = select(Bookings).where(
+        #         and_(
+        #             Bookings.room_id == room_id,
+        #             or_(
+        #                 and_(
+        #                     Bookings.date_from >= date_from,
+        #                     Bookings.date_from <= date_to
+        #                 ),
+        #                 and_(
+        #                     Bookings.date_from <= date_from,
+        #                     Bookings.date_to > date_from
+        #                 )
+        #             )
+        #         )
+        #     ).cte("booked_rooms")
+
+        #     get_rooms_left = (
+        #         select(
+        #             (Rooms.quantity - func.count(booked_rooms.c.room_id)
+        #             .filter(booked_rooms.c.room_id.is_not(None))
+        #             .label("rooms_left"))
+        #         )
+        #         .select_from(Rooms)
+        #         .join(
+        #             booked_rooms,
+        #             booked_rooms.c.room_id == Rooms.id,
+        #             isouter=True
+        #         )
+        #         .where(Rooms.id == room_id)
+        #         .group_by(Rooms.quantity, booked_rooms.c.room_id)
+        #     )
+
+        #     rooms_left = await session.execute(get_rooms_left)
+        #     rooms_left: int = rooms_left.scalar()
